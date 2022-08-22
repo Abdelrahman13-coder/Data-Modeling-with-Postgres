3f0653e3bb2adf2a2c062588f2abefb2add7a787
Abdelrahman13-coder <abdelrahman.azim98@gmail.com>
2022-08-22 14:21:29 +0200

    sql_queries_updated

--- sql_queries.py
+++ sql_queries.py
@@ -67,7 +67,7 @@ songplay_table_insert = ("""
 user_table_insert = ("""
     INSERT INTO users(user_id, first_name, last_name, gender, level)
     VALUES(%s, %s, %s, %s, %s)
-    ON CONFLICT user_id
+    ON CONFLICT (user_id)
     DO UPDATE SET level = EXCLUDED.level;
 """)
 
